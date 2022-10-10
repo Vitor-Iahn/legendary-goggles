8cd0932e83e30986e26fa27a36b3d69ad82b2e70
Vitor Frederick Iahn <97486809+Vitor-Iahn@users.noreply.github.com>
2022-10-10 00:16:24 -0300

    Add files via upload

---
+++ Dissecando uma variável.py
@@ -0,0 +1,16 @@
+a = input('Digite algo')
+print('É um digito?', a.isdigit())
+print('O texto está em caixa alta?', a.isupper())
+print('É alpha-numérico?', a.isalnum())
+print('É um caracter especial? :)', a.isascii())
+print('É numérico apenas?', a.isnumeric())
+print('É algum tipo de digito?', a.isdigit())
+print('É um número decimal?', a.isdecimal())
+print('É algum tipo de identificador?', a.isidentifier())
+print('Está em letras minúsculas apenas?', a.islower())
+print('Da para imprimir isso?', a.isprintable())
+print('É apenas espaços?', a.isspace())
+print('Está "capitalizada"? Isso é, letras maiúsculas e minúsculas', a.istitle())
+
+# Estes são apenas alguns dos "is" que existem, eu ainda não sei a útilidade de todos, sei que representam uma
+# característica à variável.
